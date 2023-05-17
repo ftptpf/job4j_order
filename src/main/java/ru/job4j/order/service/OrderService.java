31ba48ebb9cde26680828ecd0055a253a6f2b2df
ftptpf <inboxru@yandex.ru>
2023-05-17 09:06:42 +0300

    Kafka

--- src/main/java/ru/job4j/order/service/OrderService.java
+++ src/main/java/ru/job4j/order/service/OrderService.java
@@ -1,6 +1,7 @@
 package ru.job4j.order.service;
 
 import lombok.AllArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import ru.job4j.order.model.Order;
 import ru.job4j.order.repository.OrderRepository;
@@ -12,9 +13,12 @@ import java.util.Optional;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
 
     public Order save(Order order) {
-        return orderRepository.save(order);
+        Order savedOrder = orderRepository.save(order);
+        kafkaTemplate.send("job4j_orders", savedOrder);
+        return savedOrder;
     }
 
     public Optional<Order> findById(Integer id) {
